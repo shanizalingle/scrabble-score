dc3d1b110a3f66c9e5050e6fbfb48129aa7dd2e8
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-22 09:09:39 -0700

    add initial files
    Co-authored-by: Harold Mesa <harold.mesa@icloud.com>

---
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  public class ScrabbleTests
+  {
+
+    // Test methods will go here.
+
+  }
+}
